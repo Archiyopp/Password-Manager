c7445b03175e519dbe500149de63cee4962f038f
Archiyopp <crafernandezbe@unal.edu.co>
2022-10-09 00:40:39 -0500

    refactor: use match instead of expect

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,17 +14,19 @@ use tauri_plugin_sql::{Migration, MigrationKind, TauriSql};
 fn hash(password: String) -> String {
     let password_bytes = password.as_bytes();
     let salt = SaltString::generate(&mut OsRng);
-    let argon2 = Argon2::default();
 
-    argon2
-        .hash_password(password_bytes, &salt)
-        .expect("Error hashing password")
-        .to_string()
+    match Argon2::default().hash_password(password_bytes, &salt) {
+        Ok(hash) => hash.to_string(),
+        Err(_) => panic!("Error hashing password: {:?}", password),
+    }
 }
 
 #[tauri::command]
 fn verify(hashed_password: String, password: String) -> bool {
-    let parsed_hash = PasswordHash::new(&hashed_password).expect("problem using password hash");
+    let parsed_hash = match PasswordHash::new(&hashed_password) {
+        Ok(hash) => hash,
+        Err(_) => panic!("Error parsing hash: {:?}", hashed_password),
+    };
     let password_bytes = password.as_bytes();
 
     Argon2::default()
@@ -44,8 +46,10 @@ fn encrypt(password: String) -> String {
 #[tauri::command]
 fn decrypt(encrypted: String) -> String {
     let mc = new_magic_crypt!(MAGIC_KEY, 256);
-    mc.decrypt_base64_to_string(encrypted)
-        .expect("error decrypting")
+    match mc.decrypt_base64_to_string(&encrypted) {
+        Ok(password) => password,
+        Err(_) => panic!("Error decrypting password from: {:?}", encrypted),
+    }
 }
 
 fn main() {
